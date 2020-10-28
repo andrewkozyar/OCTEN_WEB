4c0da39dddc165249a003bca0ca989249e57ba58
andrewkozyar <andrew.kozyar@gmail.com>
2020-10-28 18:18:41 +0200

    new commit

---
+++ JS/task1/part1/script.js
@@ -0,0 +1,415 @@
+
+// ========= 1
+/*
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let flag1 = true;
+let flag2 = false;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(flag1);
+console.log(flag2);
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+alert(flag1);
+alert(flag2);
+
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(str4);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+document.write(flag1);
+document.write(flag2);
+*/
+
+
+// ========= 2
+/*
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let flag1 = true;
+let flag2 = false;
+
+str1 = 'welcome';
+str2 = 'my';
+str3 = 'friend';
+str4 = '!!!';
+num1 = 8;
+num2 = 4532;
+num3 = 0;
+num4 = 2.2;
+num5 = 18;
+num6 = -18;
+num7 = -1000000;
+flag1 = false;
+flag2 = true;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(flag1);
+console.log(flag2);
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+alert(flag1);
+alert(flag2);
+
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(str4);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(num5);
+document.write(num6);
+document.write(num7);
+document.write(flag1);
+document.write(flag2);
+*/
+
+
+// ========= 3
+/*
+const str1='hallo';
+const str2='world';
+const str3='!!!';
+const num1=134;
+const num2=0.5;
+const num3=-27;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(num1);
+alert(num2);
+alert(num3);
+
+document.write(str1);
+document.write(str2);
+document.write(str3);
+document.write(num1);
+document.write(num2);
+document.write(num3);
+*/
+
+
+// ========= 4
+/*
+let surname = prompt('Призвіще');
+let name = prompt('Імя');
+let fathername = prompt('По-батькові');
+
+console.log(surname);
+console.log(name);
+console.log(fathername);
+
+alert(surname);
+alert(name);
+alert(fathername);
+
+document.write(surname);
+document.write(name);
+document.write(fathername);
+*/
+
+
+// ========= Каждое выражение вложить в свою переменную
+/*
+5 < 6 -> true
+5 > 6 -> false
+5 >= 6 -> false
+5 == 6 -> false
+10 == 10 -> true
+10 >= 10 -> true
+10 != 10 -> false
+10 < 10 -> false
+10 > 10 -> false
+123 === '123' -> false
+123 == '123' -> true
+*/
+ 
+
+// ========= Дополнительно
+/*
+console.log(132 > 100 && 45 < 12 );   -> false
+console.log(34 > 33 && 23 < 90 );   -> true
+console.log(99 > 100 && 45 > 12 );   -> false
+console.log(132 > 100 || 45 < 12 );   -> true
+console.log(111 > 11 || 45 < 111 );   -> true
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );   -> true
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );   -> true
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );   -> false
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));   -> true
+console.log(!!'-1');   -> ?
+console.log(!!-1);   -> ?
+console.log(!!'0');   -> ?
+console.log(!!'null');   -> ?
+console.log(!!'undefined');   -> ?
+console.log(!!(3/'owu'));   -> ?
+console.log((111 > 11 || 45 < 111) ||  !!'0');   -> ?
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));   -> ?
+*/
+
+
+// ========= CLASS
+
+
+// ========= 1
+/*
+let str = 'Привет';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log (typeof str);
+console.log (typeof num);
+console.log (typeof flag);
+console.log (typeof txt);
+*/
+
+
+// ========= 2
+/*
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+
+console.log (a1);
+console.log (a2);
+console.log (a3);
+console.log (a4);
+console.log (a5);
+*/
+
+
+// ========= 3
+/*
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+console.log (a6);
+console.log (a7);
+console.log (a8);
+console.log (a9);
+console.log (a10);
+*/
+
+
+// ========= 4
+/*
+let height = 23;
+let width = 10;
+let s = (height * width);
+console.log (s);
+*/
+
+
+// ========= 5
+/*
+let heightC = 10;
+let dC = 4;
+let v = 10*((4/2)**2)*3.14;
+console.log (v);
+*/
+
+
+// ========= 6
+/*
+let n = 3;
+let m = 4;
+let k = Math.sqrt(n**2 + m**2);
+console.log(k);
+*/
+
+
+// ========= 7
+/*
+let str = 'Hello world';
+document.write(str);
+alert(str);
+console.log(str);
+*/
+
+
+// ========= 8
+/*
+let str = 'Kozyar A. R.\n20\nGuitar';
+alert(str);
+*/
+
+
+// ========= 9
+/*
+let str1 = 'Кто ';
+let str2 = 'ты ';
+let str3 = 'такой?';
+let concatenation = str1 + str2 + str3;
+console.log(concatenation);
+*/
+
+
+// ========= 10
+/*
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>"); // => 205
+document.write(str - a + "<br/>"); // => 15
+document.write(str * "2" + "<br/>"); // => 40
+document.write(str / 2 + "<br/>"); // => 10
+*/
+
+
+// ========= 11
+
+// parseInt("3.14")
+// parseInt("-7.875")
+// parseInt("435")
+// parseInt("Вася")
+
+
+
+// ========= 12
+/*
+let str = prompt("Enter something", "ho-ho")
+console.log(str);
+*/
+
+
+// ========= 13
+/*
+let num1 = prompt('enter number 1');
+let num2 = prompt('enter number 2');
+let sum = num1 + num2;
+console.log(sum);
+*/
+
+
+// ========= 14
+/*
+let str1 = prompt('Введіть імя і фамілію');
+let str2 = prompt('Введіть вік');
+let message = 'Доброго вечера ' + str1 + ', мои поздравления что вам ' + str2;
+alert(message)
+*/
+
+
+
+// ========= ДОП
+
+
+// ========= 1
+/*
+let num1 = prompt('num 1');
+let num2 = prompt('num 2');
+let num3 = prompt('num 3');
+
+if(num1<num2 && num1<num3){
+    console.log(num1 + ' ');
+} else if(num2<num1 && num2<num3){
+    console.log(num2 + ' ');
+} else if(num3<num1 && num3<num2){
+    console.log(num3 + ' ');
+}
+
+if((num1<num2 && num1>num3)||(num1>num2 && num1<num3)){
+    console.log(num1 + ' ');
+} else if((num2<num1 && num2>num3)||(num2>num1 && num2<num3)){
+    console.log(num2 + ' ');
+} else if((num3<num1 && num3>num2)||(num3>num1 && num3<num2)){
+    console.log(num3 + ' ');
+}
+
+if(num1>num2 && num1>num3){
+    console.log(num1 + ' ');
+} else if(num2>num1 && num2>num3){
+    console.log(num2 + ' ');
+} else if(num3>num1 && num3>num2){
+    console.log(num3 + ' ');
+}
+*/
+
+
+
+
+
+
+
+
+''
